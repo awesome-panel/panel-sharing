085980abd0459829ca191b2ad94718ed27bcc72b
Marc Skov Madsen <masma@orsted.com>
2022-11-20 05:30:43 +0000

    add test for set_directory

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,9 @@
 """We have utility functions"""
-from panel_sharing.utils import APP_KEY_PARAMETER, _get_app_key
+from pathlib import Path
+
+import pytest
+
+from panel_sharing.utils import APP_KEY_PARAMETER, _get_app_key, set_directory
 
 
 def test_get_app_key_set():
@@ -7,3 +11,18 @@ def test_get_app_key_set():
     key = "guest/basic"
     session_args = {APP_KEY_PARAMETER: [key.encode(encoding="utf8")]}
     assert _get_app_key(APP_KEY_PARAMETER, session_args) == key
+
+
+def test_set_directory_can_handle_exception(tmpdir):
+    """We can temporarily change the cwd
+
+    We will also get back to original cwd even though a exception was raised during the change
+    """
+    before = Path.cwd().absolute()
+
+    with pytest.raises(ValueError):
+        with set_directory(Path(tmpdir)):
+            assert Path.cwd().absolute() == Path(tmpdir)
+            raise ValueError("an error occured")
+
+    assert Path.cwd().absolute() == before
